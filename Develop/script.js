d5b6edb5d96a74dbe1eb559757cb0719deec87a2
Caspento <Mrcaspento@gmail.com>
2020-10-31 15:00:58 -0400

    making user prompts and vars

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var caps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var special = ['#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@'];
+var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +12,44 @@ function writePassword() {
 
   passwordText.value = password;
 
+};
+//Down below I have the user prompts asking the specifics of how long and how many charcters it will use, and character types
+function userPrompts() {
+  var length = parseInt(prompt('How long do you want your password to be?'));
+  var userInput = prompt('How many characters do you want your password to have?');
+  var includeNumeric = confirm("");
+  var includeCaps = confirm("");
+  
+  if (userInput < 8) {
+    alert('Try again, It needs to be at least 8 characters.');
+  };
+  if (userInput >= 128) {
+    alert('Try again, It needs to be less then 128 characters.');
+  };
+  
+  for (var i = userInput; userInput < 8 || userInput > 128; userInput = prompt('how long do you want your password to be?')) {
+    alert('Try again, It needs to be at least 8 charcters but less then 128 charcters.')
+  };
+  if (length > 128) {
+    alert('Try again, It needs to be less then 128 characters.')
+    return ;
+  };
+  if (length < 8) {
+    alert('Try again, It needs to be at least 8 charcters long.')
+    return;
+  };
 }
 
+
+
+////// come back and clean this up
+// after these prompts comes the spec charcter prompts
+
+
+
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", userPrompts);
+console.log(writePassword());
